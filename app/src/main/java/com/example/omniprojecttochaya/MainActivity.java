f73c97db333b93ee6b91ed13ecf3dac8488d455f
Nagaraj1027 <nagaraj.pambhala@gmail.com>
2019-02-24 10:03:09 +0530

    static fragments

--- app/src/main/java/com/example/omniprojecttochaya/MainActivity.java
+++ app/src/main/java/com/example/omniprojecttochaya/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 
 import com.example.omniprojecttochaya.activitylifecycle.OneActivity;
 import com.example.omniprojecttochaya.explicitintent.FirstActivity;
+import com.example.omniprojecttochaya.fragment.StaticFragmentsActivity;
 import com.example.omniprojecttochaya.implicitintent.ImplicitIntent;
 import com.example.omniprojecttochaya.omniclass.OmniConstants;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn1,btn2,btn3;
+    Button btn1, btn2, btn3, btn4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +27,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void initViews() {
-        btn1=(Button) findViewById(R.id.btn1);
-        btn2=(Button) findViewById(R.id.btn2);
-        btn3=(Button) findViewById(R.id.btn3);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
 
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
         btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn1:
                 explictIntent();
                 break;
@@ -46,22 +50,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn3:
                 activityLifecycle();
                 break;
+            case R.id.btn4:
+                gotoStaticFragments();
+                break;
         }
     }
 
-    void explictIntent(){
+    void explictIntent() {
 
-        Intent to_first=new Intent(MainActivity.this, FirstActivity.class);
-        to_first.putExtra(OmniConstants.KEY_NAME,"Hello Hyderabad");
-        to_first.putExtra(OmniConstants.KEY_NAME2,"Hello Bangalore");
+        Intent to_first = new Intent(MainActivity.this, FirstActivity.class);
+        to_first.putExtra(OmniConstants.KEY_NAME, "Hello Hyderabad");
+        to_first.putExtra(OmniConstants.KEY_NAME2, "Hello Bangalore");
         startActivity(to_first);
     }
-    void implicitIntent(){
-        Intent to_implicit=new Intent(MainActivity.this, ImplicitIntent.class);
+
+    void implicitIntent() {
+        Intent to_implicit = new Intent(MainActivity.this, ImplicitIntent.class);
         startActivity(to_implicit);
     }
-    void activityLifecycle(){
-        Intent to_actlifecycle=new Intent(MainActivity.this, OneActivity.class);
+
+    void activityLifecycle() {
+        Intent to_actlifecycle = new Intent(MainActivity.this, OneActivity.class);
+        startActivity(to_actlifecycle);
+    }
+
+    void gotoStaticFragments() {
+        Intent to_actlifecycle = new Intent(MainActivity.this, StaticFragmentsActivity.class);
         startActivity(to_actlifecycle);
     }
 }
