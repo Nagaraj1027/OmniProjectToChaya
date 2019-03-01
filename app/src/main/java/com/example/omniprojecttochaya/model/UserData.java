9360c23c9aab5176dee2b56dc1c89c1408db7848
Chaya <chaya@kineticreality.com>
2019-03-01 10:01:03 -0800

    Build.gradle changes

---
+++ app/src/main/java/com/example/omniprojecttochaya/model/UserData.java
@@ -0,0 +1,25 @@
+package com.example.omniprojecttochaya.model;
+
+public class UserData {
+
+    String city;
+    String Country;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+
+}
